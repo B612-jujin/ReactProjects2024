fd066a4234b79c2ebe1e6692d29d7234f8e14d54
B612-jujin <jujingood@gmail.com>
2024-09-05 16:21:42 +0900

    React chp4 Clock TEST

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './chp04/Button.css'
 // import 'chp03/Book.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library from "./chp03/Library";
+import Clock from "./chp04/Clock";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
-
+setInterval(()=> {
+    root.render(
+        <React.StrictMode>
+            <Clock/>
+        </React.StrictMode>
+    );
+},1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
